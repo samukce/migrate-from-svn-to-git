8f53a71ece0004591d4533dcdf8328ed6d0ae015
Samuel Pereira <samuel.p.araujo@gmail.com>
2016-01-28 21:28:13 -0300

    Refactor

--- Test.Core/MigrateSourceToGitTest.cs
+++ Test.Core/MigrateSourceToGitTest.cs
@@ -6,12 +6,20 @@ using NUnit.Framework;
 namespace Test.Core {
     [TestFixture]
     public class MigrateSourceToGitTest {
+        private MigrationOrchestrator migrationOrchestrator;
+        private ICreateCloneGit createCloneGit;
+        private ICreateBareGit createBareGit;
+
+        [SetUp]
+        public void Setup() {
+            createCloneGit = Substitute.For<ICreateCloneGit>();
+            createBareGit = Substitute.For<ICreateBareGit>();
+
+            migrationOrchestrator = new MigrationOrchestrator(createCloneGit, createBareGit);
+        }
+
         [Test]
         public void WhenToMigrateShouldCloneSvnToGit() {
-            var createCloneGit = Substitute.For<ICreateCloneGit>();
-
-            var migrationOrchestrator = new MigrationOrchestrator(createCloneGit, Substitute.For<ICreateBareGit>());
-
             migrationOrchestrator.Migrate(string.Empty, string.Empty, string.Empty);
 
             createCloneGit.ReceivedWithAnyArgs(1)
@@ -20,10 +28,6 @@ namespace Test.Core {
 
         [Test]
         public void WhenToMigrateShouldCreateBare() {
-            var createBareGit = Substitute.For<ICreateBareGit>();
-
-            var migrationOrchestrator = new MigrationOrchestrator(Substitute.For<ICreateCloneGit>(), createBareGit);
-
             migrationOrchestrator.Migrate(string.Empty, string.Empty, string.Empty);
 
             createBareGit.ReceivedWithAnyArgs(1)
@@ -32,11 +36,6 @@ namespace Test.Core {
 
         [Test]
         public void ShouldFirstCallCreateCloneFromSvn() {
-            var createBareGit = Substitute.For<ICreateBareGit>();
-            var createCloneGit = Substitute.For<ICreateCloneGit>();
-
-            var migrationOrchestrator = new MigrationOrchestrator(createCloneGit, createBareGit);
-
             migrationOrchestrator.Migrate(string.Empty, string.Empty, string.Empty);
 
             Received.InOrder(() => {
@@ -47,26 +46,16 @@ namespace Test.Core {
 
         [Test]
         public void AbortIfErrorWhenCreateCloneGit() {
-            var createBareGit = Substitute.For<ICreateBareGit>();
-            var createCloneGit = Substitute.For<ICreateCloneGit>();
-
             createCloneGit.WhenForAnyArgs(c => c.Create(string.Empty, string.Empty, string.Empty))
                           .Throw<Exception>();
-
-            var migrationOrchestrator = new MigrationOrchestrator(createCloneGit, createBareGit);
 
             Assert.Throws<Exception>(() => migrationOrchestrator.Migrate(string.Empty, string.Empty, string.Empty));
         }
 
         [Test]
         public void AbortIfErrorWhenCreateBareGit() {
-            var createBareGit = Substitute.For<ICreateBareGit>();
-            var createCloneGit = Substitute.For<ICreateCloneGit>();
-
             createBareGit.WhenForAnyArgs(c => c.Create(string.Empty))
                          .Throw<Exception>();
-
-            var migrationOrchestrator = new MigrationOrchestrator(createCloneGit, createBareGit);
 
             Assert.Throws<Exception>(() => migrationOrchestrator.Migrate(string.Empty, string.Empty, string.Empty));
         }
