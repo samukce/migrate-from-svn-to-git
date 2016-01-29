cc86a77f65043f94052f91d115b3e027f962af0f
Samuel Pereira <samuel.p.araujo@gmail.com>
2016-01-28 23:31:00 -0300

    Validate if file exist.

---
+++ Core/ValidateFile.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using MigrateFromSvnToGit.Interfaces;
+
+namespace MigrateFromSvnToGit {
+    public class ValidateFile : IValidateFile {
+        public bool Exist(string fullPath) {
+            return File.Exists(fullPath);
+        }
+    }
+}
---
+++ Test.Core/ValidateFileTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using MigrateFromSvnToGit;
+using NUnit.Framework;
+
+namespace Test.Core {
+    [TestFixture]
+    public class ValidateFileTest {
+        [Test]
+        public void ShouldReturnTrueWhenFileExist() {
+            var filename = Path.GetTempFileName();
+
+            Assert.That(new ValidateFile().Exist(filename));
+        }
+
+        [Test]
+        public void ShouldReturnFalseWhenFileDoesNotExist() {
+            Assert.That(new ValidateFile().Exist("fileDoesNotExist.txt"), Is.False);
+        }
+    }
+}
